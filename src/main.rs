cab5fb4acc48d886e518693af6906f96e1d280fa
Christopher Roberts <djabsynth@gmail.com>
2023-01-02 09:58:25 -0800

    updated for refs

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels", area(rect));
+    println!("The area of the rectangle is {} square pixels", area(&rect));
 }
 
-fn area(rectangle: Rectangle) -> u32 {
+fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
