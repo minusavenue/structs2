676ad62b768c027c0999b983e65750e564538114
Christopher Roberts <djabsynth@gmail.com>
2023-01-02 10:43:13 -0800

    impl blocks and associated functions

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,19 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -16,7 +29,23 @@ fn main() {
         height: 50,
     };
 
+    let rect1 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+
+    let rect2 = Rectangle {
+        width: 40,
+        height: 50,
+    };
+
+    let rect3 = Rectangle::square(4);
+
+    println!("rect can hold rect1: {}", rect.can_hold(&rect1));
+    println!("rect can hold rect1: {}", rect.can_hold(&rect2));
+
     println!("rect: {:?}", rect);
 
     println!("The area of the rectangle is {} square pixels", rect.area());
+    println!("The area of the square is {} pixels", rect3.area());
 }
